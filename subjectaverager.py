95dea9bfaed5770dc0aa67778e0e0bd6825b6e60
Angelo <markclemz86@gmail.com>
2025-10-19 12:45:01 +0800

    Update subjectaverager.py
    input how many subjects you want and input the grade to each of the subject and it will average it

--- subjectaverager.py
+++ subjectaverager.py
@@ -1,16 +1,19 @@
-print("=====Grade Averager=====")
-grades = []
-subjects = int(input("How many subjects?: "))
-
-for i in range(1, subjects + 1):
-    grade = float(input(f"Enter grade of the subject {i}: "))
-    grades.append(grade)
-    average = sum(grades) / subjects
+while True:
+    print("=====Grade Averager=====")
+    grades = []
+    subjects = int(input("How many subjects?: "))
     
-    print("\nGrades entered:")
-    for idx, grade in enumerate(grades, start=1):
-        print(f"Subject {idx}: {grade}")
-        print(f"\nAverage grade: {average}")
-
-
-         
+    for i in range(1, subjects + 1):
+        grade = float(input(f"Enter grade of the subject {i}: "))
+        grades.append(grade)
+        
+        
+    average = sum(grades) / subjects
+    print(f"\nYour average grade is: {average:.2f}")
+    
+    again = input("Do you wish to calculate again? (y/n): ")
+    if again != 'y':
+        print("Goodbye...")
+    else: 
+        print("Invalid input")
+        break
